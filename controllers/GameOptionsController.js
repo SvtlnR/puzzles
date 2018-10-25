60de46e290d38452747f67231041f778e5042946
SvtlnR <svet.ragulina@gmail.com>
2018-10-25 16:53:41 +0300

    Image path

--- controllers/GameOptionsController.js
+++ controllers/GameOptionsController.js
@@ -2,7 +2,7 @@ puzzlesApp.controller("GameOptionsController", function($scope, $location, puzzl
 	$scope.optionsAmount = [9, 16, 25];
 	$scope.selectedAmount = 9;
 	$scope.start = function() {
-		imageCrop.cutImage(Math.sqrt($scope.selectedAmount), Math.sqrt($scope.selectedAmount), "../images/img1.jpg").then(function(imgPieces) {
+		imageCrop.cutImage(Math.sqrt($scope.selectedAmount), Math.sqrt($scope.selectedAmount), "/images/img1.jpg").then(function(imgPieces) {
 			puzzlesService.savePuzzles(imgPieces);
 			puzzlesService.saveOptions(Math.sqrt($scope.selectedAmount), Math.sqrt($scope.selectedAmount));
 			puzzlesService.clearPositions();
