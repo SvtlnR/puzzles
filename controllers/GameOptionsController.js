32bbca9735f165c15fe329a0a299da195e0f7848
SvtlnR <svet.ragulina@gmail.com>
2018-10-25 17:38:00 +0300

    Choosing picture

--- controllers/GameOptionsController.js
+++ controllers/GameOptionsController.js
@@ -1,8 +1,10 @@
 puzzlesApp.controller("GameOptionsController", function($scope, $location, puzzlesService, imageCrop) {
 	$scope.optionsAmount = [9, 16, 25];
+	$scope.pics=["./images/img1.jpg","./images/img2.jpeg"];
+	$scope.selectedImage = $scope.pics[0];
 	$scope.selectedAmount = 9;
 	$scope.start = function() {
-		imageCrop.cutImage(Math.sqrt($scope.selectedAmount), Math.sqrt($scope.selectedAmount), "./images/img1.jpg").then(function(imgPieces) {
+		imageCrop.cutImage(Math.sqrt($scope.selectedAmount), Math.sqrt($scope.selectedAmount), $scope.selectedImage).then(function(imgPieces) {
 			puzzlesService.savePuzzles(imgPieces);
 			puzzlesService.saveOptions(Math.sqrt($scope.selectedAmount), Math.sqrt($scope.selectedAmount));
 			puzzlesService.clearPositions();
